08a18fe37713ef0f8c7c6e55b9eb43fea142a98a
MRSA <sanggisda@gmail.com>
2024-03-21 13:58:14 +0700

    add: use service in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react'
+
 import Navbar from './components/Navbar'
 import Container from './components/Container'
 import Loading from './components/Loading'
 import Error from './components/Error'
 
+import { getNews } from './services/getNews'
+
 function App() {
+  // const [state, setstate] = useState(initialState);
+
+  useEffect(() => {
+    const fetchTechNews = async () => {
+      const res = await getNews({
+        searchQuery: 'microsoft'
+      })
+      console.log(res)
+    }
+
+    fetchTechNews()
+
+    return () => {
+      
+    }
+  }, [])
+  
+
   return (
     <>
       <Navbar/>

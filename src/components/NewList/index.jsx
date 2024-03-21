cd435821167742f14efc8ba3dcbbe17038511cb0
MRSA <sanggisda@gmail.com>
2024-03-21 14:54:32 +0700

    add: newscard component

--- src/components/NewList/index.jsx
+++ src/components/NewList/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './NewList.module.css'
+import NewsCard from '../NewsCard'
 
 const NewList = ({ articles }) => {
   return (
     <div className={styles.NewList}>
       {articles.map((article, index, arr) => {
         return (
-          <div key={index}>
-            <h1>{article.title}</h1>
-          </div>
+          <NewsCard
+            key={index}
+            src={article.urlToImage}
+            title={article.title}
+            publishedAt={article.publishedAt}
+            author={article.author}
+            sourceName={article.source.name}
+            description={article.description}
+            url={article.url}
+            notLastChild={!(arr.length === index + 1)}
+          />
         )
       })}
     </div>

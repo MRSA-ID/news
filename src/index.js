9915166f4630ddd096a035d58e46d4f29785b34d
MRSA <sanggisda@gmail.com>
2024-03-21 15:20:40 +0700

    add: improvement App js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   Route
 } from 'react-router-dom'
 import './index.css';
-import App from './App';
+import App from './pages/App';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,27 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-import Navbar from './components/Navbar'
-import Container from './components/Container'
-import Loading from './components/Loading'
-import Error from './components/Error'
-import NewList from './components/NewList'
+import Navbar from '../components/Navbar'
+import Container from '../components/Container'
+import Loading from '../components/Loading'
+import Error from '../components/Error'
+import NewList from '../components/NewList'
 
-import { getNews } from './services/getNews'
+import { getNews } from '../services/getNews'
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const { id } = useParams()
+  const DEFAULT_SEARCH_QUERY = 'microsoft'
+
   useEffect(() => {
     const fetchTechNews = async () => {
       setLoading(true)
       const res = await getNews({
-        searchQuery: 'microsoft'
+        searchQuery:  id || DEFAULT_SEARCH_QUERY
       })
       if(!res){
         setLoading(false)
@@ -33,7 +37,7 @@ function App() {
     return () => {
       
     }
-  }, [])
+  }, [id])
   
 
   return (
